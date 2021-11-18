b53328bb55b19f93d299b55da8f28c8d42488796
Tyler Hablutzel <tyhablutzel@yahoo.com>
2021-11-17 22:33:32 -0700

    Completed Messaging

--- src/application/CheckMessages.java
+++ src/application/CheckMessages.java
@@ -4,10 +4,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -17,24 +21,29 @@ import java.sql.ResultSet;
 import org.sqlite.SQLiteDataSource;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 public class CheckMessages {
     @FXML
     private TableView<Conversation> Table;
+    
 
     public void initialize() {
-    	initializeTable();
+    	initializeMessageTable();
     }
     
-    private void initializeTable() {
+    
+    private void initializeMessageTable() {
     	SQLiteDataSource ds = new SQLiteDataSource();
     	ds.setUrl("jdbc:sqlite:info.db");
     	ResultSet rs = null;
     	PreparedStatement ps = null;
     	try {
     	Connection con = ds.getConnection();
-    	String sql = "SELECT Receiver, Messages from MessagesDB where Sender = 'User'";
+    	String user = Main.user.getFirstName() + " " + Main.user.getLastName();
+    	System.out.println(user);
+    	String sql = "SELECT Receiver, Messages from MessagesDB where Sender = '" + user + "'";
     	ps = con.prepareStatement(sql);
     	rs = ps.executeQuery();
     	while(rs.next()) {
@@ -58,7 +67,16 @@ public class CheckMessages {
 
     @FXML
     private void back() throws IOException {
-        Scene newScene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Patient Portal.fxml"))));
+    	Scene newScene;
+    	if (Main.user.getType() == "Patient") {
+    		newScene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Patient Portal.fxml"))));
+    	} else if (Main.user.getType() == "Doctor") {
+    		newScene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Doctor Portal.fxml"))));
+    	} else if (Main.user.getType() == "Nurse") {
+    		newScene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Nurse Portal.fxml"))));
+    	} else {
+    		newScene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml"))));
+    	}
         Stage window = (Stage) Table.getScene().getWindow();
         window.setScene(newScene);
     }
--- src/application/NewMessage.java
+++ src/application/NewMessage.java
@@ -3,8 +3,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import org.sqlite.SQLiteDataSource;
 
@@ -31,14 +34,72 @@ public class NewMessage {
 	@FXML
 	private ChoiceBox<String> docNurseDropdown = new ChoiceBox<>();
 
-    private String[] placeholderNames = {"John Smith", "Pete Williams", "Michael Scott"};
+    private ArrayList<String> receiverNames = new ArrayList<String>();
 	String toText;
 	String message = "";
 	
 	
 	public void initialize() {
-		docNurseDropdown.getItems().addAll(placeholderNames);
+		insertTable();
+		docNurseDropdown.getItems().addAll(receiverNames);
 		docNurseDropdown.setOnAction(this::getName);
+	}
+	
+	public void insertTable() {
+		SQLiteDataSource ds = new SQLiteDataSource();
+    	ds.setUrl("jdbc:sqlite:info.db");
+    	ResultSet rs = null;
+    	PreparedStatement ps = null;
+    	try {
+    		Connection con = ds.getConnection();
+    		String sql = "";
+    		String name;
+    		if (Main.user.getType() =="Doctor") {
+    			sql = "SELECT b.First_name, b.Last_name from PatientInfoDb b, DoctorInfoDb c where b.DoctorID = c.Id AND c.Id = " + Main.user.getId();
+    			ps = con.prepareStatement(sql);
+    	    	rs = ps.executeQuery();
+    	    	while(rs.next()) {
+    	    		name = rs.getString("First_name") + " " + rs.getString("Last_name");
+    	    		System.out.println(name);
+    	    		receiverNames.add(name);
+    	    	}
+    		} else if (Main.user.getType() =="Nurse") {
+    			sql = "SELECT b.First_name, b.Last_name from PatientInfoDb b, NurseInfoDb c where b.Nurse = c.Id AND c.Id = " + Main.user.getId();
+    			ps = con.prepareStatement(sql);
+    	    	rs = ps.executeQuery();
+    	    	while(rs.next()) {
+    	    		name = rs.getString("First_name") + " " + rs.getString("Last_name");
+    	    		System.out.println(name);
+    	    		receiverNames.add(name);
+    	    	}
+    		} else if (Main.user.getType() =="Patient") {
+    			sql = "SELECT b.First_name, b.Last_name from DoctorInfoDb b, PatientInfoDb c where c.DoctorID = b.Id AND c.Id = " + Main.user.getId();
+    			ps = con.prepareStatement(sql);
+    	    	rs = ps.executeQuery();
+    	    	while(rs.next()) {
+    	    		name = rs.getString("First_name") + " " + rs.getString("Last_name");
+    	    		System.out.println(name);
+    	    		receiverNames.add(name + "(Doctor)");
+    	    	}
+    	    	sql = "SELECT b.First_name, b.Last_name from NurseInfoDb b, PatientInfoDb c where c.Nurse = b.Id AND c.Id = " + Main.user.getId();
+    	    	ps = con.prepareStatement(sql);
+    	    	rs = ps.executeQuery();
+    	    	while(rs.next()) {
+    	    		name = rs.getString("First_name") + " " + rs.getString("Last_name");
+    	    		System.out.println(name);
+    	    		receiverNames.add(name + "(Nurse)");
+    	    	}
+    		}
+    	} catch (SQLException e) {
+    		System.out.println(e.toString());
+    	} finally {
+    		try {
+    			rs.close();
+    			ps.close();
+    		} catch(SQLException e) {
+    			System.out.println(e.toString());
+    		}
+    	}
 	}
 	
 	public void getName(ActionEvent event) {
@@ -52,7 +113,7 @@ public class NewMessage {
 			Stage stage = (Stage) sendMessageButton.getScene().getWindow();
 			stage.close();
 		} else {
-			insertMessage("User", toText, message);
+			insertMessage(Main.user.getFirstName() + " " + Main.user.getLastName(), toText, message);
 			Stage stage = (Stage) sendMessageButton.getScene().getWindow();
 			stage.close();
 		}
